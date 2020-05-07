6b970cfd9bb65024eaa541d7bb678f44eb1b9603
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-07 21:17:45 +0100

    crud rest

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use stric';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -11,6 +15,26 @@ const route = router.get('/', (req, res, next) => {
         version: "0.0.2"
     });
 });
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({ 
+        id: id, 
+        item: req.body 
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
