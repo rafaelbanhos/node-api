121c46eba4a79c1c77cf062a6f8b232acb7bd927
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-07 20:35:32 +0100

    separating server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+'use stric';
+
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API Up",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
+
+module.exports = app;
