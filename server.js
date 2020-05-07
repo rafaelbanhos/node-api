1aef0eace19b6421d0003a570ebd663cf9d8d148
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-07 20:14:28 +0100

    starting debug

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
-
+server.on('listening', onListening);
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
@@ -59,4 +59,12 @@ function onError(error) {
         default:
             throw error;
     }    
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe' + addr
+        : 'port' + addr.port;
+    debug('Listening on ' + bind); 
 }
