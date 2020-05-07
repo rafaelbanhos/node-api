b64aebce754bc4a1142b2dd9f8fd8a957ecc6527
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-07 17:29:38 +0100

    creating initial route

--- server.js
+++ server.js
@@ -3,3 +3,19 @@
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
+
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+var route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
