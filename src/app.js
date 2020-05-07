b4e1a2b91c64bde19255faa3a1cc35989bc22770
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-07 20:57:45 +0100

    configuring nodemon

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API Up",
-        version: "0.0.1"
+        version: "0.0.2"
     });
 });
 app.use('/', route);
