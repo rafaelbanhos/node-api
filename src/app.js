b2de7e98ed09a80c188ee6599026f514365af98e
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-07 22:22:55 +0100

    fix routes

--- src/app.js
+++ src/app.js
@@ -6,35 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API Up",
-        version: "0.0.2"
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({ 
-        id: id, 
-        item: req.body 
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
---
+++ src/routes/index-route.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({ 
+        title: "Node Store API", 
+        version: "0.0.2" })
+});
+
+module.exports = router;
