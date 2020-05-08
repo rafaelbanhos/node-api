64605ecc85e7688b4e5b543086302bb401c783d1
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-08 18:37:29 +0100

    implementing asynchronous methods

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -3,41 +3,44 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 
-exports.get = () => {
-    return Product
-        .find({
-            active: true
-        }, 'title price slug');
+exports.get = async () => {
+    const res = await Product.find({
+        active: true
+    }, 'title price slug');
+    return res;
 }
 
-exports.getBySlug = (slug) => {
-    return Product
+exports.getBySlug = async (slug) => {
+    const res = await Product
         .findOne({
             slug: slug,
             active: true
         }, 'title description price slug tags');
+    return res;
 }
 
-exports.getById = (id) => {
-    return Product
+exports.getById = async (id) => {
+    const res = await Product
         .findById(id);
+    return res;
 }
 
-exports.getByTag = (tag) => {
-    return Product
+exports.getByTag = async (tag) => {
+    const res = await Product
         .find({
             tags: tag,
             active: true
         }, 'title description price slug tags');
+    return res;
 }
 
-exports.create = (data) => {
+exports.create = async (data) => {
     var product = new Product(data);
-    return product.save();
+    await product.save();
 }
 
-exports.update = (id, data) => {
-    return Product
+exports.update = async (id, data) => {
+    await Product
         .findByIdAndUpdate(id, {
             $set: {
                 title: data.title,
@@ -48,7 +51,7 @@ exports.update = (id, data) => {
         });
 }
 
-exports.delete = (id) => {
-    return Product
+exports.delete = async (id) => {
+    await Product
         .findOneAndRemove(id);
 }
