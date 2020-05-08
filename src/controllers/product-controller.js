49604f22f7c8932ca1b0f97ef85a9108921f2549
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-08 14:23:15 +0100

    get products by slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -7,7 +7,20 @@ exports.get = (req, res, next) => {
     Product
         .find({
             active: true
-        }, 'title price, slug')
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/product-controller');
 
 router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
