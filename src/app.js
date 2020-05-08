2e3c9e587ac4f73906698a4db2dba85ec1f393ff
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-08 11:09:40 +0100

    creating a product

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ const router = express.Router();
 //conecta ao banco
 mongoose.connect('mongodb+srv://banhaum:teste@cluster0-dehfn.mongodb.net/test?retryWrites=true&w=majority');
 
+//carrega os models
+const Product = require('./models/product');
+
 //carrega as rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,14 +1,28 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+    product
+        .save()
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso!'});
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao cadastrar produto!', 
+            data: e});
+    });
+    
 };
 
 exports.put = (req, res, next) => {
     const id = req.params.id;
-    res.status(200).send({ 
-        id: id, 
-        item: req.body 
+    res.status(200).send({
+        id: id,
+        item: req.body
     });
 };
 
