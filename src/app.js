9258cdecb039e845923f9f23eebf2c62faba16bb
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-11 10:43:50 +0100

    add customer model

--- src/app.js
+++ src/app.js
@@ -8,10 +8,11 @@ const app = express();
 const router = express.Router();
 
 //conecta ao banco
-mongoose.connect('mongodb+srv://banhaum:<password>@node-api-dehfn.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://banhaum:powered85@node-api-dehfn.mongodb.net/test?retryWrites=true&w=majority');
 
 //carrega os models
 const Product = require('./models/product');
+const Customer = require('./models/customer');
 
 //carrega as rotas
 const indexRoute = require('./routes/index-route');
