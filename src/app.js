91b35f8e587085753c7f3bfdfec4ed62bd3b686d
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-11 11:15:53 +0100

    creating customer controller and fixing routes

--- src/app.js
+++ src/app.js
@@ -18,11 +18,13 @@ const Order = require('./models/order');
 //carrega as rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
+app.use('/customers', customerRoute);
 
 module.exports = app;
