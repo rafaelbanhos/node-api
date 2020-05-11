6fb1ac864c3daa90e2a098acdb9625daa2342365
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-11 12:06:19 +0100

    creating configs

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = "";
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb+srv://banhaum:powered85@node-api-dehfn.mongodb.net/test?retryWrites=true&w=majority',
+    sendgridKey: 'TBD',
+    containerConnectionString: 'TBD'
+}
