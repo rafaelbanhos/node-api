1a05e385283e997dab51fd1311de368a9cf86abe
rafaelbanhos <rafaelbanhos@gmail.com>
2020-05-13 10:32:05 +0100

    refresh token

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -74,3 +74,37 @@ exports.authenticate = async (req, res, next) => {
         });
     }
 };
+
+exports.refreshToken = async (req, res, next) => {
+    try {
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];      
+        const data = await authService.decodeToken(token);
+
+        const customer = await repository.getById(data.id);
+
+        if (!customer) {
+            res.status(404).send({
+                message: 'Cliente não encontrado'
+            });
+            return;
+        }
+
+        const tokenData = await authService.generateRoken({
+            id: customer._id,
+            email: customer.email,
+            name: customer.name
+        });
+
+        res.status(201).send({
+            token: token,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+};
--- src/routes/customer-route.js
+++ src/routes/customer-route.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/customer-controller');
+const authService = require('../services/auth-service');
 
 router.post('/', controller.post);
 router.post('/authenticate', controller.authenticate);
+router.post('/resfresh-token', authService.authorize, controller.refreshToken);
     
 module.exports = router;
